5c7facc94ee3c945f9d5657ec7295634469abf36
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2026-08-24 14:56:19 +0700

    perf(telescope): enable native FZF sorter

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -26,9 +26,18 @@ return function()
 				hidden = true,
 			},
 		},
+		extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+		},
 	})
 
 	-- Load extensions
+	require("telescope").load_extension("fzf")
 	require("telescope").load_extension("bookmarks")
 
 	-- Keymaps
